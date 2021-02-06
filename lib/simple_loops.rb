724e63c88a0f7c3ad1f6278fe849f3f6d448386e
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-06 19:23:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,17 @@
-# Write your methods here
+def loop_message_five_times(message)
+  number = 0
+  
+  while number < 5 do
+    puts message 
+    number += 1
+  end
+end
+
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
+end
+
